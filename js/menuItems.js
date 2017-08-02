3d71e2d6b68477c2d5e1abeefb8474a0b2b30002
JohnHarTzion <yonatanhar@yahoo.com>
2017-08-02 15:26:49 -0400

    nav menu done and takes json file

--- js/menuItems.js
+++ js/menuItems.js
@@ -1,40 +1,38 @@
 import React from 'react';
 import {List, ListItem} from 'material-ui/List';
-import ActionInfo from 'material-ui/svg-icons/action/info';
-import Divider from 'material-ui/Divider';
 import Subheader from 'material-ui/Subheader';
 import Avatar from 'material-ui/Avatar';
 import FileFolder from 'material-ui/svg-icons/file/folder';
-import ActionAssignment from 'material-ui/svg-icons/action/assignment';
-import {blue500, yellow600} from 'material-ui/styles/colors';
-import EditorInsertChart from 'material-ui/svg-icons/editor/insert-chart';
+import preload from '../data/navBarData.json'
 
 const ListItems = () => (
   <div className="ListItems">
     <List>
       <Subheader>Folders</Subheader>
+      {preload.types.map(type => (
       <ListItem
         leftAvatar={<Avatar icon={<FileFolder />} />}
-        primaryText="Books"
+        primaryText={type.What}
         initiallyOpen={false}
         primaryTogglesNestedList={true}
         nestedItems={[
           <ListItem
           key={1}
-          primaryText="Hinduism"
+          primaryText={type.Subject}
           leftIcon={<FileFolder />}
           initiallyOpen={false}
           primaryTogglesNestedList={true}
           nestedItems={[
             <ListItem
               key={1}
-              primaryText="Yoga Sutras"
+              primaryText={type.Sub_Subject}
               leftIcon={<FileFolder />}
             />
             ]}
           />
         ]}
       />
+      ))}
     </List>
   </div>
 );
