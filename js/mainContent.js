42d842a019f854ee5bf7ef36686caf482798fbde
JohnHarTzion <yonatanhar@yahoo.com>
2017-08-15 16:31:19 -0400

    articles are getting information from AJAX request

--- js/mainContent.js
+++ js/mainContent.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import LeftNav from './LeftNav'
 import AppBar from 'material-ui/AppBar'
 import preload from '../data/articleData.json'
@@ -8,68 +8,95 @@ import Subheader from 'material-ui/Subheader';
 import Avatar from 'material-ui/Avatar';
 import FileFolder from 'material-ui/svg-icons/file/folder';
 import {blue500, yellow600} from 'material-ui/styles/colors';
+import MenuItem from 'material-ui/MenuItem';
+import axios from 'axios';
 
 
 
-class MainContent extends React.Component {
+class MainContent extends Component {
+	constructor(props) {
+		super(props)
+
+		this.state = {
+			articles:[]
+		};
+	}
+
+	componentDidMount () {
+		axios.get(`http://localhost:3000/Articles`)
+			.then((response: {data: {Article_Name: string }}) => {
+				this.setState({articles: response.data});
+				console.log(this.state.articles)
+			})
+	}
 
 	handleTouchTapLeftIconButton() {
 
 		state = {
-    navDrawerOpen: false,
-  };
+    		navDrawerOpen: false,
+  		};
 	    this.setState({
 	      navDrawerOpen: !this.state.navDrawerOpen,
 	    });
   	};
 
 	render() {
+
+		let articleComponent;
+		articleComponent = (this.state.articles.map(article=>
+
+			<ListItem 
+				className="articleComponent"
+				key={article.id}
+				leftAvatar={<Avatar icon={<FileFolder />} />}
+				primaryText={article.Article_Name}
+				secondaryText={article.Last_Updated}
+			/>
+			));
+		let openedComponent;
+		openedComponent = (this.state.articles.map(article=>
+
+			<ListItem 
+				key={article.id}
+				primaryText="Opened By:"
+				secondaryText={article.Opened_By}
+			/>
+			));
+		
 		return(
 			<div className='mainContent'>
 				<AppBar
 					title="Title"
-					iconClassNameRight="muidocs-icon-navigation-expand-more"
 					onLeftIconButtonTouchTap={this.handleTouchTapLeftIconButton}
 					iconElementRight={<LeftNav />}
 				/>
 					<List>
 						<table className="MainContentList">
-							<td>
-								<h3>Article Name</h3>
-								{preload.Articles.map(article => (
-								<ListItem
-									leftAvatar={<Avatar icon={<FileFolder />} />}
-									rightIcon={<ActionInfo />}
-									primaryText={article.Article_Name}
-									secondaryText="Jan 9, 2014"
-								/>
-								))}
-							</td>
-							<td>
-								<h3>Opened By</h3>
-								{preload.Articles.map(article => (
-								<ListItem
-									leftAvatar={<Avatar icon={<FileFolder />} />}
-									rightIcon={<ActionInfo />}
-									primaryText={article.Opened_By}
-									secondaryText="Jan 17, 2014"
-								/>
-								))}
-							</td>
-							<td>
-								<h3>Last Updated</h3>
-								{preload.Articles.map(article => (
-								<ListItem
-									leftAvatar={<Avatar icon={<FileFolder />} />}
-									rightIcon={<ActionInfo />}
-									primaryText={article.Last_Updated}
-									secondaryText="Jan 28, 2014"
-								/>
-								))}
-							</td>
-						</table>
-					</List>
+							<thead>
+								<tr>
+									<th />
+								</tr>
+							</thead>
+							<tbody>
+							<tr>
+								<td >
+									<h3 className="ArticleHeader">Article</h3>
+								</td>
+								
+							</tr>
+							<tr>
+								<td>
+									{articleComponent}
+								</td>
+								<td>
+									{openedComponent}
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</List>
 			</div>
+
 		)
 	}
 };
